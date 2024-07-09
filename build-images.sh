d7223b9a1a082aaaf045e854aba12e62be81992c
Martin Bhuong <martin@genius.ke>
2024-07-10 00:34:23 +0200

    Implement Versioning

--- build-images.sh
+++ build-images.sh
@@ -14,6 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="infisical"
+APP_VERSION="v0.75.0-postgres"
 
 # Create a new empty container image
 container=$(buildah from scratch)
@@ -45,7 +46,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/postgres:15.5-alpine3.19 docker.io/redis:7  docker.io/infisical/infisical:latest-postgres" \
+    --label="org.nethserver.images=docker.io/postgres:15.5-alpine3.19 docker.io/redis:7  docker.io/infisical/infisical:${APP_VERSION}" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
